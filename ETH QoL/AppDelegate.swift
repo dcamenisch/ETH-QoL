a1900e3c864195401a252d2045387e95f9fff948
dcamenisch <danny@camenisch.ws>
2024-06-29 13:07:54 +0200

    Initial Commit

---
+++ ETH QoL/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ETH QoL
+//
+//  Created by Danny on 29.06.2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
